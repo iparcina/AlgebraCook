8243d4f804d01cc2812fe716a58923f2508d24f8
Ivana Parcina <ivana_parcina@hotmail.com>
2017-12-04 15:15:26 +0100

    dovrseno

---
+++ app/Http/Controllers/RecipesController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Recipe;
+use App\Ingredient;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class RecipesController extends Controller {
+	
+	public function __construct() {
+		$this->middleware('auth');
+	}
+	
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('index', array('recipes' => Recipe::get()));
+    }
+
+
+    public function add()
+    {
+        return view('add');
+		//return "Prikaz view-a sa web obrascem za unos.";
+    }
+	
+	public function save(Request $request)
+	{
+        $data = $request->all();
+		$noviRecept = new Recipe;
+		$noviRecept->name = $data['name'];
+		$noviRecept->creator_id = auth()->user()->id;
+		$noviRecept->description = $data['opis'];
+		
+		if ( $noviRecept->save() ) {
+		
+			foreach($data['ingredient'] as $key => $value) {
+				
+				$sastojak = new Ingredient;
+				$sastojak->name = $value;
+				$sastojak->recipe_id = $noviRecept->id;
+				$sastojak->save();
+			}
+		}
+		
+		return redirect()->action('RecipesController@index');
+	}
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function view($id)
+    {
+        return view('view')->with('recipe', Recipe::find($id));
+    }
+
+	public function viewSastojci(){
+		
+		return view("sastojci", array( "ingredients" => Ingredient::get(), 
+										"recipes" => Recipe::get()
+										));
+	}
+	
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+		$recipe = Recipe::find($id);
+		
+		if ( $recipe->creator_id !== auth()->user()->id )
+			return redirect()->action("RecipesController@index");
+		
+        return view('edit')->with('recipe', $recipe);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $data = $request->all();
+		$recipe = Recipe::find($data['id']);
+		
+		foreach ($recipe->ingredients as $ingredient)
+			$ingredient->delete();
+			
+		$recipe->name = $data['name'];
+		$recipe->description = $data['opis'];
+		
+		if ( $recipe->save() ) {
+			
+			foreach ( $data['ingredient'] as $key => $value ) {
+				
+				$sastojak = new Ingredient;
+				$sastojak->name = $value;
+				$sastojak->recipe_id = $recipe->id;
+				$sastojak->save();
+			}
+		}
+		return redirect()->action('RecipesController@index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+		Recipe::find($id)->delete();
+		return redirect()->action('RecipesController@index');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,59 @@
 |
 */
 
+// Authentication routes
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::post('/auth/login', 'Auth\AuthController@postLogin');
+Route::get('/auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes
+Route::get('/auth/register', 'Auth\AuthController@getRegister');
+Route::post('/auth/register', 'Auth\AuthController@postRegister');
+
+Route::get('/profile', 'UsersController@profile');
+Route::post('/profile', 'UsersController@profile');
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/recipes');
 });
+
+Route::get('/home', function () {
+    return redirect('/recipes');
+});
+
+Route::get('/sastojci', 'RecipesController@viewSastojci');
+
+Route::get('/recipes', 'RecipesController@index'); // prilaz svih recepata (popis)
+
+Route::get('/recipes/add', 'RecipesController@add'); // prikaz view-a sa web obrascem za unos
+Route::post('/recipes/add', 'RecipesController@save'); // spremanje podataka u bazu
+
+Route::get('/recipes/view/{id}', 'RecipesController@view'); // detaljan prikaz određenog recepta
+
+Route::get('/recipes/edit/{id}', 'RecipesController@edit'); // prikaz webobrasca za uređivanje
+Route::post('/recipes/edit', 'RecipesController@update'); // ažuriranje podataka u bazi
+
+Route::get('recipes/del/{id}', 'RecipesController@delete'); // brisanje recepta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
